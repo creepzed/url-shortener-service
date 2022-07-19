960f77bd8194c3090795a98ff9926d99a7e52722
Rodrigo Cuevas M <rcuevasm@ripley.com>
2022-07-18 23:47:24 -0400

    add main.go

--- app/main.go
+++ app/main.go
@@ -1,7 +1,18 @@
 package main
 
-import "github.com/creepzed/url-shortener-service/app/shared/infrastructure/log"
+import (
+	"github.com/creepzed/url-shortener-service/app/shared/infrastructure/rest"
+	"time"
+)
+
+const (
+	host      = ""
+	port      = 8080
+	dbTimeOut = 5 * time.Second
+)
 
 func main() {
-	log.Debug("Hello World!")
+	echo := rest.New()
+
+	echo.Logger.Fatal(echo.StartServer(rest.Setup(host, port)))
 }
