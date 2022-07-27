309d1748ae1af25cca9b3cc63a0635e5b248433c
Rodrigo Cuevas Morales <rodrigo.cuevas.morales@gmail.com>
2022-07-27 19:57:28 -0400

    WIP openapi (#19)
    Co-authored-by: Rodrigo Cuevas M <rcuevasm@ripley.com>

--- app/shared/infrastructure/rest/server.go
+++ app/shared/infrastructure/rest/server.go
@@ -28,7 +28,7 @@ const (
 // @license.name Apache 2.0
 // @license.url http://www.apache.org/licenses/LICENSE-2.0.html
 
-// @host localhost:8080
+// @host http://meli.creepzed.com:8080/
 // @BasePath /
 
 func New() *echo.Echo {
